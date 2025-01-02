0beda19729c921887bd4fe4651e584a5d8ea0e92
Mayank02iit <141387619+Mayank02iit@users.noreply.github.com>
2025-01-02 22:37:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from fastapi.staticfiles import StaticFiles
 app.mount("/birthdayboyy", StaticFiles(directory=r"C:\Users\Mayank\Desktop\aryanbirthday\project\static",html=True), name="firstpage")
 app.mount('/aryan_my_nigga',StaticFiles(directory=r"C:\Users\Mayank\Desktop\aryanbirthday\project\first_page",html=True), name="secondpage")
 
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return """
+    <h1>Welcome to Aryan's Birthday Celebration!</h1>
+    <p>Visit <a href="/birthdayboyy">Birthday Page</a></p>
+    """
